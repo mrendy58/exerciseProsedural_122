1af863d17a6a4584b07a7ce602bfc84611c04aa1
mrendy58 <derendyyy58@gmail.com>
2023-03-27 09:55:21 +0700

    membuat status rerata 2

--- exerciseProsedural_122/exerciseProsedural_122.cpp
+++ exerciseProsedural_122/exerciseProsedural_122.cpp
@@ -13,12 +13,22 @@ string status(double c) {
 }
 
 string status2(double rata, double nil) {
-
+    if (rata <= 70 || nil > 80)
+        return "Diterima";
+    else
+        return "Ditolak";
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double nilM, nilB;
+    cout << "Masukan nilai Matematika :";
+    cin >> nilM;
+    cout << "Masukan nilai Bahasa Inggris :";
+    cin >> nilB;
+    cout << "\nstatus kelulusan =" << status(rerata(nilM, nilB));
+    cout << "\nstatus kelulusa ke 2 =" << status(rerata(nilM, nilB));
+    return 0;
 }
 
 
